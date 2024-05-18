80adaf12350a273bb1b5643adc6da4a6663b7a8e
Тимофей Матвеев <matveev-timofey@mail.ru>
2024-05-18 13:12:16 +0300

    12

---
+++ App5/SelectionPageFlyout.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App5
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SelectionPageFlyout : ContentPage
+    {
+        public ListView ListView;
+
+        public SelectionPageFlyout()
+        {
+            InitializeComponent();
+
+            BindingContext = new SelectionPageFlyoutViewModel();
+            ListView = MenuItemsListView;
+        }
+
+        private class SelectionPageFlyoutViewModel : INotifyPropertyChanged
+        {
+            public ObservableCollection<SelectionPageFlyoutMenuItem> MenuItems { get; set; }
+
+            public SelectionPageFlyoutViewModel()
+            {
+                MenuItems = new ObservableCollection<SelectionPageFlyoutMenuItem>(new[]
+                {
+                    new SelectionPageFlyoutMenuItem { Id = 0, Title = "Page 1" },
+                    new SelectionPageFlyoutMenuItem { Id = 1, Title = "Page 2" },
+                    new SelectionPageFlyoutMenuItem { Id = 2, Title = "Page 3" },
+                    new SelectionPageFlyoutMenuItem { Id = 3, Title = "Page 4" },
+                    new SelectionPageFlyoutMenuItem { Id = 4, Title = "Page 5" },
+                });
+            }
+
+            #region INotifyPropertyChanged Implementation
+            public event PropertyChangedEventHandler PropertyChanged;
+            void OnPropertyChanged([CallerMemberName] string propertyName = "")
+            {
+                if (PropertyChanged == null)
+                    return;
+
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+            #endregion
+        }
+    }
+}
---
+++ App5/SelectionPageFlyoutMenuItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App5
+{
+    public class SelectionPageFlyoutMenuItem
+    {
+        public SelectionPageFlyoutMenuItem()
+        {
+            TargetType = typeof(SelectionPageFlyoutMenuItem);
+        }
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public Type TargetType { get; set; }
+    }
+}
